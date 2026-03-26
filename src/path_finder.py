7fc424c3b4aca783d8d81a5356aea97834766c27
Lichen13 <161976633+lichen13@users.noreply.github.com>
2026-03-26 22:07:46 +0000

    Stop tracking changes to logs/tool.log

--- src/path_finder.py
+++ src/path_finder.py
@@ -1,10 +1,11 @@
 import os
 
-HOME_PATH = os.path.expanduser("~")
+HERE = os.path.dirname(os.path.abspath(__file__))
+ROOT = os.path.abspath(os.path.join(HERE, os.pardir)) # os.pardir is the equivalent of ".." which means "go up one directory"
 
 FILES = {
-    "menu_template": os.path.join(HOME_PATH, "hack_tool_proj", "src", "templates", "main_menu.txt"),
-    "port_scanner_template": os.path.join(HOME_PATH, "hack_tool_proj", "src", "templates", "port_scanner_menu.txt"),
-    "full_scan_list": os.path.join(HOME_PATH, "hack_tool_proj", "src", "scanner", "lists", "full_scan.txt"),
-    "quick_scan_list": os.path.join(HOME_PATH, "hack_tool_proj", "src", "scanner", "lists", "quick_scan.txt")
+    "menu_template": os.path.join(ROOT, "src", "templates", "main_menu.txt"),
+    "port_scanner_template": os.path.join(ROOT, "src", "templates", "port_scanner_menu.txt"),
+    "full_scan_list": os.path.join(ROOT, "src", "scanner", "lists", "full_scan.txt"),
+    "quick_scan_list": os.path.join(ROOT, "src", "scanner", "lists", "quick_scan.txt")
 }
