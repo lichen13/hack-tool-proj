844665bbf669fbd464f8621c1ec34b78223b98fd
Lichen13 <161976633+lichen13@users.noreply.github.com>
2026-03-30 04:19:21 +0000

    New function 'startup': Show the main menu & verify the os. Some options in the menu (like port scanner) are not working yet.

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import requests
+
+def startup():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        print("HackToolProj only works on a Linux based system.")
+        exit()
+
+    print("HackToolProj - By: Lichen")
+
+    while True:
+        print("\nMenu:")
+        print("0. Exit")
+        print("1. Port Scanner ( not working yet )")
+        optionTaken = input("\nOption taken: ").strip()
+
+        if optionTaken == "0":
+            os.system("clear")
+            print("\nSee you soon.\n")
+            exit()
+        elif optionTaken == "1":
+            os.system("clear")
+            print("\nPort Scanner is not accessible yet.")
+        else:
+            os.system("clear")
+            print("\nInvalid option, Please try again.")
+
+startup()
